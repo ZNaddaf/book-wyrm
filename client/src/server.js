8deca059a3b3209c7773f8704a5a91ed5c5a15b4
kimiinglet <kinglet91391@gmail.com>
2020-02-28 11:00:36 -0600

    server.js

--- client/src/server.js
+++ client/src/server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const { join } = require("path");
 const morgan = require("morgan");
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./../../routes")
 
-const port = process.env.SERVER_PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.static(join(__dirname, "build")));
@@ -13,4 +15,35 @@ app.use((_, res) => {
   res.sendFile(join(__dirname, "build", "index.html"));
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+//If server doesn't work uncomment out line 19 and ocmment out 21-23
+// app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+
+//Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist")
+.then(() => console.log("Database is connected"))
+.catch(err => console.log(err));
+
+
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
