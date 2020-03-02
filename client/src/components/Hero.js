a64cfed78121bc17db9333079ea93a3d03507901
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-03-01 18:06:20 -0600

    branding content added: logo, banner, botm

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import logo from "../assets/book_wyrm_banner.png";
-import botm from "../assets/botm_mint.png";
+import banner from "../assets/book_wyrm_banner.png";
+// import botm from "../assets/botm_mint.png";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    <img className="mb-3 app-banner" src={logo} alt="wyrm banner" />
+    <img className="mb-3 app-banner" src={banner} alt="wyrm banner" />
     <h1 className="mb-15">Welcome, fellow Book Wyrm!</h1>
     <h2>Log in or sign up to view and save your personalized reading list.</h2>
 
