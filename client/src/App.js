a2d165c3f109fc815cc5c15e37372eb698594012
Z Naddaf <znaddaf@protonmail.com>
2020-03-02 14:04:30 -0600

    Navbar updated style config added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-// import Footer from "./components/Footer";
-// import Header from "./components/Header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Wrapper from "./components/Wrapper";
-import { Container } from "reactstrap";
-
 import HomePage from './pages/HomePage';
 import LandingPage from './pages/LandingPage';
-// import LogIn from "./pages/LogIn";
-// import SignUp from "./pages/SignUp";
 import Search from "./pages/Search";
 import Profile from "./pages/Profile";
-
 import PrivateRoute from "./components/PrivateRoute";
 import Loading from "./components/Loading";
-import NavBar from "./components/nav";
+import NavBar from "./components/navbar";
 import Footer from "./components/Footer";
-// import Home from "./views/Home";
-// import Profile from "./views/Profile";
 import { useAuth0 } from "./react-auth0-spa";
 import history from "./utils/history";
+// import OldLibraryWallpaper from "./assets/OldLibraryWallpaper.png"
 
 
 // styles
@@ -40,16 +31,14 @@ const App = () => {
     <Router history={history}>
       <div id="app">
         <NavBar />
-        <Container className="flex-grow-1 mt-5 bg-green-200">
+        <div className="flex-grow-1">
           <Switch>
             <Route exact path="/home" component={HomePage} />
-            {/* <Route exact path="/login" component={LogIn} />
-            <Route exact path="/signup" component={SignUp} /> */}
             <Route exact path="/search" component={Search} />
             <PrivateRoute path="/profile" component={Profile} />
             <Route path="/" exact component={LandingPage} />
           </Switch>
-        </Container>
+        </div>
         <Footer />
       </div>
     </Router>
--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -4,7 +4,7 @@ import banner from "../assets/book_wyrm_banner.png";
 // import botm from "../assets/botm_mint.png";
 
 const Hero = () => (
-  <div className="text-center hero my-5">
+  <div className="text-center hero">
     <img className="mb-3 app-banner" src={banner} alt="wyrm banner" />
     <h1 className="mb-15">Welcome, fellow Book Wyrm!</h1>
     <h2>Log in or sign up to view and save your personalized reading list.</h2>
