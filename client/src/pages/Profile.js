4663c544ed2a02b3768d529d1a5433096cf1e3b5
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-03-03 14:19:27 -0600

    no unsecure profile pic

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -18,13 +18,6 @@ const Profile = () => {
 
     <Container className="mb-5">
       <Row className="align-items-center profile-header mb-5 text-center text-md-left">
-        <Col md={2}>
-          <img
-            src={user.picture}
-            alt="Profile"
-            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-          />
-        </Col>
         <Col md>
           <h2>{user.name}</h2>
           <p className="lead text-muted">{user.email}</p>
