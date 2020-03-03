f226780d689436b7f32e4316aa29c4a8ab66e3a4
Z Naddaf <znaddaf@protonmail.com>
2020-03-03 14:34:31 -0600

    auth0 fix

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,32 +1,32 @@
 const db = require("../models");
-// import { useAuth0 } from "../react-auth0-spa";
+import { useAuth0 } from "../react-auth0-spa";
 
 
 module.exports = {
-    findAll: function (req, res) {
-        const { user } = useAuth0();
-        console.log(req.query)
-        db.Book.find(req.query)
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    },
-    findById: function (req, res) {
-        db.Book.findById(req.params.id)
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    },
-    create: function (req, res) {
-        console.log(req.body);
-        db.Book.create(req.body)
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    },
+  findAll: function (req, res) {
+    const { user } = useAuth0();
+    console.log(req.query)
+    db.Book.find(req.query)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Book.findById(req.params.id)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    console.log(req.body);
+    db.Book.create(req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
 
-    remove: function (req, res) {
-        console.log("bam")
-        db.Book.findById({ _id: req.params.id })
-            .then(dbBook => dbBook.remove())
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    }
+  remove: function (req, res) {
+    console.log("bam")
+    db.Book.findById({ _id: req.params.id })
+      .then(dbBook => dbBook.remove())
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  }
 }
