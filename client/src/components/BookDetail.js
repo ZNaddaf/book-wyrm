6ff312b328f7d7ef59b35bd8dc62b87bcd573b03
Z Naddaf <znaddaf@protonmail.com>
2020-03-04 11:40:19 -0600

    book divs updated

--- client/src/components/BookDetail.js
+++ client/src/components/BookDetail.js
@@ -2,22 +2,20 @@ import React from 'react';
 const URL = "https://covers.openlibrary.org/b/id/";
 const size = "-M.jpg";
 function BookDetail(props) {
-    // console.log(props)
-    return (
-        <div className="text-center ">
-                <div style={{ marginTop: "10px", border: "solid", width: "250px" }}>
-              <img src={URL + props.cover_i + size} style={{ margin: "auto" }} alt="" />
-              <div className="addBookBtn"><strong>Title: </strong>{props.title_suggest}</div>
-              <div><strong>Author: </strong>{props.author_name}</div>
-              <div><strong>Year Published: </strong>{props.first_publish_year}</div>
-              <div><strong>ID: </strong>{props.cover_i}</div>
-              <button className="addBookBtn" onClick={(event) => {
-                event.preventDefault();
-                props.handleBtnClick(props)
-              }}>Add Book</button>
-            </div>
-        </div>
-    )
+  // console.log(props)
+  return (
+    <div className="text-center w-48 h-64 m-16">
+      <img src={URL + props.cover_i + size} style={{ margin: "auto" }} alt="" />
+      <div className="addBookBtn"><strong>Title: </strong>{props.title_suggest}</div>
+      <div><strong>Author: </strong>{props.author_name}</div>
+      <div><strong>Year Published: </strong>{props.first_publish_year}</div>
+      <div><strong>ID: </strong>{props.cover_i}</div>
+      <button className="addBookBtn" onClick={(event) => {
+        event.preventDefault();
+        props.handleBtnClick(props)
+      }}>Add Book</button>
+    </div>
+  )
 }
 
 export default BookDetail
