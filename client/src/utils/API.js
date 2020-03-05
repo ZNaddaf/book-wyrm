c0eeae202bf19b975b52abc6fa673d9aee42e39a
kimiinglet <kinglet91391@gmail.com>
2020-03-05 09:46:43 -0600

    Author btn working

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,18 @@
 import axios from "axios";
 const BASEURL = "https://openlibrary.org/search.json?title=";
+//Author search
+const BASEURL2 = "https://openlibrary.org/search.json?author=";
 
 export default {
   search: function (query) {
     return axios.get(BASEURL + query);
   },
+  //Author
+  searchAuthor: function (query) {
+    return axios.get(BASEURL2 + query);
+  },
+
+
   // Gets all books
   getBooks: function () {
     return axios.get("/api/books");
